82075eeb3016e1231b8cdcd3a17eecb61f5558e3
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-06-21 21:00:10 +0800

    Third commit: created a method to save the img. added return to each method and removed unecessary arguments.

--- qr_generator.py
+++ qr_generator.py
@@ -7,12 +7,14 @@ class QRGenerator:
 
     def enter_data(self):
         data = input("Enter the text or URL to encode in QR code: ")
+        return data
 
     def enter_filename(self):
         enter_filename = input("Enter filename: ")
         filename = f"{enter_filename}.png"
+        return filename
 
-    def generate_qr(self, fill, back, data, filename):
+    def generate_qr(self, data):
         qr = qrcode.QRCode(
             version=1,
             error_correction = qrcode.constants.ERROR_CORRECT_H,
@@ -21,3 +23,10 @@ class QRGenerator:
         )
         qr.add_data(data)
         qr.make(fit=True)
+
+        img = qr.make_image(fill_color=self._fill_color, back_color=self._back_color)
+        return img
+
+    def save_img(self, filename, img):
+        img.save(filename)
+        print(f"QR code successfully generated as {filename}")
