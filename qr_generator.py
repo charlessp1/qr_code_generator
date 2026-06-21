2d2c1c80c0124e30c2a36bfa783ddc7200e132a1
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-06-21 20:43:12 +0800

    Second commit: created a method to enter the filename and generate the qr

--- qr_generator.py
+++ qr_generator.py
@@ -1,9 +1,23 @@
 import qrcode
 
 class QRGenerator:
-    def __init__(self, data, fill="black", back="white"):
+    def __init__(self, fill="black", back="white"):
         self._fill_color = fill
         self._back_color = back
 
-    def input_data(self):
+    def enter_data(self):
         data = input("Enter the text or URL to encode in QR code: ")
+
+    def enter_filename(self):
+        enter_filename = input("Enter filename: ")
+        filename = f"{enter_filename}.png"
+
+    def generate_qr(self, fill, back, data, filename):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction = qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
